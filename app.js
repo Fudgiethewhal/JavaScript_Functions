b625a9d90cf70548bbd942955bcead8c96f8c176
Bryseida Hernandez <bryseida.hernandez@yahoo.com>
2025-05-04 00:59:18 -0700

    completed one exercise

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-console.log("Hello World!\n==========\n");
-
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+function printOdds(count) {
+    if (count < 0) {
+        for (let i = 0; i >= count; i--) {
+            if (i % 2 !== 0) {
+                console.log(i);
+            }
+        }
+    } else {
+        for (let i = 1; i <= count; i++) {
+            if (i % 2 != 0) {
+                console.log(i);
+            }
+        }
+    }
+}
 
+printOdds(-10); 
+printOdds(10);
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+function checkAge(name, age) {
+    let oldEnoughMsg = `Congrats ${name}! You are old enough to drive!`;
+    let tooYoungMsg = `Sorry ${name}, you are too young to drive ${
+        16 - age
+    } year(s) until you can drive.`;
+    
+    if (age < 16) {
+        console.log(tooYoungMsg);
+    } else {
+        console.log(oldEnoughMsg);
+    }
+}
+checkAge("Ben", 12)
+checkAge("Seth", 16)
